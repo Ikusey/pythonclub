e8c157e09c1fcc0282154997b5252d67df31c6a8
Azriel Policarpio <52266762+Ikusey@users.noreply.github.com>
2020-01-22 22:45:34 -0800

    uploads pythonclub and club folders
    adds correct postgres password to settings.py in python club, fixes urls.py in pythonclub and adds urls.py to club folder

--- pythonclub/settings.py
+++ pythonclub/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'pythonclubdb',
         'USER' : 'postgres',
-        'PASSWORD': 'p@ssw0rd1',
+        'PASSWORD': 'p@ssword1',
         'HOST' :'localhost',
         'PORT' :'',
     }
